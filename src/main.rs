d1df041b1d9042b3fb3f6256cb5e2e024a1be63d
lucasjm <lucasmartin@hey.com>
2024-01-08 19:18:23 +1030

    feat: all prompts scaffolded

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,64 @@
-use clap::Parser;
-use std::io::{BufRead, Lines, Stdin, StdinLock};
-
-#[derive(Parser)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    #[arg(short, long, default_value_t = String::from(""))]
-    name: String,
-}
+use inquire::{DateSelect, Select, Text};
+use std::process::exit;
 
 fn main() {
-    // TODO: implement next session - https://github.com/mikaelmello/inquire
-
-    let args: Args = Args::parse();
-
+    // 1. show a welcome message
     println!("Welcome to Remembrall 🧙");
+    println!(
+        "Let's start by logging something you finished watching, reading, or playing recently."
+    );
 
-    let mut name: String = args.name.clone();
+    // 2. Prompt to select a type of media from a list (enum)
+    let media_types = vec!["Movie", "Game", "Series", "Book"];
 
-    if name.is_empty() {
-        println!("What's your name?");
-        let stdin: Stdin = std::io::stdin();
-        let mut iterator: Lines<StdinLock<'_>> = stdin.lock().lines();
-        name = iterator.next().unwrap().unwrap();
+    let media_type = Select::new("What type of Media have you completed?", media_types)
+        .prompt()
+        .unwrap();
 
-        println!("Hello, {}!", name);
-    } else {
-        println!("Hello, {}!", name)
+    // 3. Text prompt for the name of the media
+    let title = Text::new("What is the title of the media you have finished?")
+        .prompt()
+        .unwrap();
+
+    // 3.1 Optional description text prompt.
+    let description = Text::new("(Optional) Add a description of the media you have finished?")
+        .prompt_skippable()
+        .unwrap();
+
+    // 4. Prompt for date of completion (default to now)
+    let date = DateSelect::new("When did you finish the media?")
+        .prompt()
+        .unwrap();
+
+    // 5. Rating from 1 to 10, could probably do this as Text prompt
+    // let rating_options: Vec<u8> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    // let rating: Option<u8> = Select::new("", rating_options).prompt_skippable().unwrap();
+
+    // 6. Show media in a data list and prompt for confirmation
+    println!("Title: {}", title);
+    println!("Type: {}", media_type);
+    match description {
+        Some(d) => Some({
+            println!("Description: {}", d);
+        }),
+        None => None,
+    };
+    println!("Completed Date: {}", date);
+
+    // 7. On confirm, insert into database.
+    let confirm_options = vec!["Cancel", "Save"];
+    let confirmation = Select::new("Would you like to save this media?", confirm_options).prompt();
+
+    match confirmation {
+        Ok("Cancel") => {
+            println!("Cancelled");
+            exit(0)
+        }
+        Ok("Save") => save(),
+        _ => println!("Something went wrong, please restart the program."),
     }
+}
+
+fn save() {
+    println!("Please wait, saving in progress!");
 }
